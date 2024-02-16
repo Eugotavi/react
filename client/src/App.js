e87822b4275aed6febe05dabd2ec559b9f904ab9
Eugotavi <158354226+Eugotavi@users.noreply.github.com>
2024-02-16 21:40:20 +0000

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Featured from './components/Featured';
+import TopNav  from './components/TopNav'
+function App() {
+  return (
+    <div className="App">
+      <TopNav/>
+      <Featured/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Featured.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const Featured = () => {
+    const sliders = [
+        {
+
+        }
+    ]
+    const [currentIndex, setCurrentIndex] = useState(0)
+  return (
+        <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative'>
+            <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+                style={{backgroundImage: }}
+            ></div>
+        </div>
+    )
+}
+
+export default Featured
